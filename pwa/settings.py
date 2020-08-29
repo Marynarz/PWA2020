447375589f5710c88a83fe1f75ff158fc045b443
206074 <nwojtek@gmail.com>
2020-08-29 12:03:19 +0200

    setings

--- pwa/settings.py
+++ pwa/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'pwa.wsgi.application'
 DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'pwa_db.db',
     }
 }
 
